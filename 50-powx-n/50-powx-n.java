ebc7cb5bf87360c42f2564c8c46cdbda996e17d4
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2022-03-27 23:34:59 +0530

    Time: 1 ms (81.91%), Space: 42.5 MB (63.69%) - LeetHub

---
+++ 50-powx-n/50-powx-n.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double myPow(double x, double n) {
+            long l = Math.abs((long) n);
+            double ans = 1;
+            while (l > 0) {
+                if ((l & 1) == 1)
+                    ans *= x;
+                x *= x;
+                l >>= 1;
+            }
+            return n < 0 ? 1 / ans : ans;
+        }
+}
