ec1e53505fddcbe6b27487c0f9767c8d914a08d6
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2022-03-28 23:13:02 +0530

    Time: 6 ms (99.96%), Space: 41.6 MB (90.57%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x == 0){
+            return true;
+        }
+        if(x < 0 || x % 10 == 0){
+            return false;
+        }
+    
+    
+    int rev = 0;
+    while(x > rev){
+        int dig = x % 10;
+        x = x/10;
+        rev =  (rev * 10) + dig ;
+    }
+    
+    if(x == rev || x == rev / 10){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+}
