52b5b81eaffb0a0265f82a3ee51692d6ed3b4f59
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2022-08-29 00:00:19 +0530

    Time: 1 ms (100.00%), Space: 43.3 MB (93.32%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -4,7 +4,6 @@ class Solution {
         int count=0;//this keeps the count of elements in frst row & 1st col
         int m=mat[0].length;
         int i=0,k=0;
-//this while loop will be sending all the starting element-index of the diagonal
          while(count<m+n){
              if(i==n-1&&k<m){++k;}
              if(i!=n-1){i++;}
@@ -15,7 +14,7 @@ class Solution {
     }
     public void pass(int [][]a,int row,int col,int n,int m){
  
-        //here we perform COUNT SORT IN EVERY DIAGONAL     
+            
        
         int []arr=new int [101]; //Frequency Array
         int i=row,j=col;
